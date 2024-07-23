9525a064ec82bdbe6294864746d9e4dc224acc2c
Ho1ocron <cmrfreeze@gmail.com>
2024-07-23 20:13:31 +0500

    added db.py

---
+++ src/database.py
@@ -0,0 +1,12 @@
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from sqlalchemy.orm import Session, sessionmaker
+from sqlalchemy import URL, create_engine, text
+from config import Settings
+
+
+engine = create_engine(url=Settings.DATABASE_URL_psycopg, echo=True, pool_size=5, max_overflow=10)
+                
+with engine.connect() as conn:
+    res = conn.execute(text("Select vers"))
+    print(f"{res=}")
+                
