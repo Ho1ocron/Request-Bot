497f5ad2c4ce3553b9a01ed29df147f1db70b144
Yar <108415700+Ho1ocron@users.noreply.github.com>
2025-05-11 19:37:09 +0500

    m

--- src/database/models.py
+++ src/database/models.py
@@ -31,6 +31,12 @@ class Group(Model):
 
 
 class GroupNotFoundError(Exception):
-    def __init__(self, group_id):
+    def __init__(self, group_id: int | str) -> None:
         super().__init__(f"Group with ID {group_id} not found.")
         self.group_id = group_id
+
+
+class UserNotFoundError(Exception):
+    def __init__(self, user_id: int | str):
+        super().__init__(f"User with ID {user_id} not found.")
+        self.user_id = user_id
