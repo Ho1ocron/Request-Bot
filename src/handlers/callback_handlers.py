6f36635ca49849ec0bb012e90e3ad8d90b71742a
Yar <108415700+Ho1ocron@users.noreply.github.com>
2025-06-22 00:51:47 +0500

    cleaned code

--- src/handlers/callback_handlers.py
+++ src/handlers/callback_handlers.py
@@ -6,10 +6,8 @@ from aiogram.types import InputMediaAnimation, InputMediaDocument, InputMediaPho
 from states import PostStates, GroupCallback, get_message_to_forward, set_message_to_forward, get_media_group_messages, save_media_group_messages
 
 
-
 router = Router(name=__name__)
 
-# callback.bot | message.bot
 
 @router.callback_query(F.data == "send_command_list")
 async def send_random_value(callback: CallbackQuery) -> None:
